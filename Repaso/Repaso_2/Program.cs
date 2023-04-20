de6e225bdfaab289fad34106e7a5e63729a41772
martinsaulo <martinsaulo77@gmail.com>
2023-04-20 17:04:27 -0300

    Primer commit repaso 2

--- Repaso/Repaso_2/Program.cs
+++ Repaso/Repaso_2/Program.cs
@@ -1,6 +1,16 @@
 ﻿// Carga Productos
 
-int[,] productos = new int[10, 2];
+int[,] productos = new int[10, 3]; //Precio, Cantidad, Ventas 
+
+for (int i = 0; i<10; i++)
+{
+    for (int j = 0; j<3; j++)
+    {
+        productos[i,j] = 0;
+    }
+}
+
+
 
 int codigoProducto, precioProducto, cantidadProducto;
 
@@ -39,13 +49,63 @@ for (int i = 0; i < 10; i++)
 
 Console.WriteLine("El producto con mayor stock es el producto nro " + productoMaxCantidad);
 
-
+Console.WriteLine("Ventas.");
 //Ventas
+int ventasProducto;
+codigoProducto = 1;
+
 do
 {
+    Console.Write("Ingrese el código del producto[1-10]: ");
+    codigoProducto = int.Parse(Console.ReadLine()!);
 
-} while ();
 
+
+    if (codigoProducto != 0) 
+    {
+        Console.Write("Ingrese las ventas del producto: ");
+        ventasProducto = int.Parse(Console.ReadLine()!);
+
+        productos[codigoProducto-1,2] = ventasProducto;
+    }
+
+} while (codigoProducto != 0);
+
+
+Console.Write("Los productos");
+
+for (int i = 1; i<=10; i++)
+{
+    if (productos[i-1,1] - productos[i-1,2] <= 0)
+    {
+        Console.Write(" " + (i));
+    }
+}
+Console.Write(" tienen una cantidad menor o igual a 0.\n");
+
+//Nombres
+
+string[] nombresProductos = new string[10];
+
+for (int i=1; i<=10; i++)
+{
+    Console.Write("Ingrese el nombre del producto nro " + i + ":  ");
+
+    nombresProductos[i - 1] = Console.ReadLine()!;
+
+}
+
+Console.Write("Los productos:");
+
+for (int i = 0; i<10; i++)
+{
+    if (productos[i, 2] < 10)
+    {
+        Console.Write(" " + nombresProductos[i]);
+    }
+}
+
+Console.Write(" tuvieron ventas menores a 10 unidades.");
 
 /* IMPRIMIR MATRIZ
 for (int fila = 0; fila < productos.GetLength(0); fila++)
